133a1d364894e54766569e7556029d2e89bfb67a
Olawale Otubu <waleoxy@gmail.com>
2022-03-24 20:45:39 +0100

    home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Layout from "../components/Layout";
+import Register from "../components/Register";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-3xl">hello world</div>
+    <Layout>
+      <Register />
+    </Layout>
   );
 }
